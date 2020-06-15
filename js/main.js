3d6f02ec07435cc092a0809a9674caafb6e6ec5c
Akinbode Akinwunmi <peezykon@.github.com>
2020-06-15 20:29:50 +0100

    com

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('form');
 form.addEventListener("submit", e => {
+	e.preventDefault();
     var email = document.getElementById('email').value;
     var pattern = /^[A-Za-z._]{3,}@[A-Za-z{3,}[.]{1}[A-Za-z.]{2,6}$/;
 
